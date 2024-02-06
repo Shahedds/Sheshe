ac31ff3fcf51f690c9e425f1bbf40c4dbda6fe14
Shahedds <143745509+Shahedds@users.noreply.github.com>
2024-02-06 22:59:11 +0300

    Add files via upload

---
+++ view 3 1.php
@@ -0,0 +1,36 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "betecdb";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: ". $conn->connect_error);
+    }
+        
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+        // collect value of input field
+        
+        $searchWord = $_REQUEST['searchWord'];
+    
+        $sqlquery = "select * from products where PName= '$searchWord'";
+    
+        $result = $conn->query($sqlquery);
+
+        if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["PID"]. " - Name: " . $row["PName"]. " , Price: " . $row["PPrice"]. "<br>";
+        }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+   
+    }
+?>
